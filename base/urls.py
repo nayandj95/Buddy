912d0697175b53589be0302db53e841a4c53a600
Nayan_Makwana <nayan.dj95@gmail.com>
2022-05-27 12:12:47 +0530

    user auth done

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 
 urlpatterns =[
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
+    path('register/', views.registerpage, name='register'),
+
     path('',views.home, name='home'),
     path('room/<str:pk>',views.room, name='room'),
+    
     path('create-room/',views.createroom, name='create-room'),
     path('update-room/<str:pk>/',views.updateroom, name='update-room'),
     path('delete-room/<str:pk>/',views.deleteroom, name='delete-room'),
--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -12,6 +18,58 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Front End Devloper'},
 
 # ]
+
+def loginpage(request):
+    
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+    context = {
+            'page' : page
+        }
+    return render(request, 'base/login_register.html',context)
+
+def logoutuser(request):
+    logout(request)
+    return redirect('home')
+
+def registerpage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Ann Error occured during registration process')
+    context = {
+        'page' : page,
+        'form' : form,
+    }
+    return render(request, 'base/login_register.html',context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -38,7 +96,7 @@ def room(request,pk):
     return render(request, 'base/room.html',context)
 
 
-
+@login_required(login_url='/login')
 def createroom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -51,10 +109,14 @@ def createroom(request):
     return render(request, 'base/room_form.html',context)
 
 
-
+@login_required(login_url='/login')
 def updateroom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You arenot authenticated')
+
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -67,9 +129,13 @@ def updateroom(request,pk):
     return render(request, 'base/room_form.html',context)
 
 
-
+@login_required(login_url='/login')
 def deleteroom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You arenot authenticated')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
